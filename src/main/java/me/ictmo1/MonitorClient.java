49f268f3557d4d6849a97c6b9bfc4f9885026f33
EsmeeKraan <esmeekraan2@gmail.com>
2022-05-17 22:09:50 +0200

    hoi 5

--- src/main/java/me/ictmo1/MonitorClient.java
+++ src/main/java/me/ictmo1/MonitorClient.java
@@ -1,19 +1,55 @@
 package me.ictmo1;
 
+import com.sun.management.OperatingSystemMXBean;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MonitorClient {
 
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("localhost", 6969);
-        var scanner = new Scanner(System.in);
         var output = socket.getOutputStream();
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
         while (true) {
-            output.write(scanner.nextLine().getBytes(StandardCharsets.UTF_8));
+            double CPUload = 0;
+
+            int sampleRate = 20;
+            for (int i = 0; i < sampleRate; i++){
+                CPUload+= osBean.getCpuLoad();
+//                Thread.sleep(1000/sampleRate);
+            }
+            String message = (CPUload/sampleRate) + " " + calucateDiskusage() + "\n";
+            output.write(message.getBytes(StandardCharsets.UTF_8));
             output.flush();
         }
+    }
+
+    public static double calucateDiskusage(){
+        double totalUsablespace = 0;
+        double totalSpace = 0;
+
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        for (Path root : FileSystems.getDefault().getRootDirectories()) {
+
+            try {
+                FileStore store = Files.getFileStore(root);
+                if(store.type().equals("CDFS"))
+                    continue;
+                totalUsablespace+= store.getUsableSpace();
+                totalSpace+= store.getTotalSpace();
+            } catch (IOException ignored) {
+            }
+        }
+        return totalUsablespace/totalSpace;
     }
 
 }
--- src/main/java/me/ictmo1/MonitorGUI.java
+++ src/main/java/me/ictmo1/MonitorGUI.java
@@ -1,23 +1,29 @@
 package me.ictmo1;
 
+import com.sun.tools.javac.Main;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.awt.*;
 import java.io.BufferedInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
-import java.util.Timer;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class MonitorGUI {
 
-    // JavaFX: grafiek te tekenen
+    static HashMap<Socket, Long> serverTime = new HashMap<>();
+
+    static JProgressBar procesbelastingProgressBar = new JProgressBar();
+    static JProgressBar beschikbaarheidProgressBar = new JProgressBar();
+    static JProgressBar diskUsageProgressBar = new JProgressBar();
+
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Graphic Tracer Monitoring");
         frame.setSize(720, 450);
 
@@ -26,8 +32,6 @@ public class MonitorGUI {
         frame.add(new JLabel("Beschikbaar"));
         frame.add(new JCheckBox());
 
-        JProgressBar procesbelastingProgressBar = new JProgressBar();
-        procesbelastingProgressBar.setString("Procesbelasting %");
         procesbelastingProgressBar.setStringPainted(true);
         procesbelastingProgressBar.setMaximum(100);
 
@@ -35,50 +39,30 @@ public class MonitorGUI {
 
         frame.add(new JLabel("Diskruimte"));
 
-        JProgressBar beschikbaarheidProgressBar = new JProgressBar();
-        beschikbaarheidProgressBar.setString("Beschikbaarheid %");
         beschikbaarheidProgressBar.setStringPainted(true);
         beschikbaarheidProgressBar.setMaximum(100);
 
         frame.add(beschikbaarheidProgressBar, "skip 1");
 
-        frame.add(new JLabel("Uptime"), "skip 3, wrap");
+        frame.add(new JLabel("Uptime"), "skip 3");
+        frame.add(new JTextField(3), "wrap");
 
-        /* bende met tijd converten */
 
-//        java.util.Timer timer = new Timer();
-//        timer.scheduleAtFixedRate(new TimerTask()
-//        {
-//            public void run()
-//            {
-//                int second, minute, hour;
-//                Calendar date = Calendar.getInstance();
-//                second = date.get(Calendar.SECOND);
-//                minute = date.get(Calendar.MINUTE);
-//                hour = date.get(Calendar.HOUR);
-//                frame.add(new JLabel("Current time is  " + hour + " : " +
-//                        minute +" : " + second), "wrap, \r");
-//            }
-//        }, 1 * 5000, 1 * 5000);
-
-        JProgressBar diskUsageProgressBar = new JProgressBar();
-        diskUsageProgressBar.setString("Gebruikt %");
         diskUsageProgressBar.setStringPainted(true);
         diskUsageProgressBar.setMaximum(100);
 
-
-        new Thread(() -> {
-            while (true) {
-                diskUsageProgressBar.setValue(new Random().nextInt(0, 100));
-                beschikbaarheidProgressBar.setValue(new Random().nextInt(0, 100));
-                procesbelastingProgressBar.setValue(new Random().nextInt(0, 100));
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+//        new Thread(() -> {
+//            while (true) {
+//                diskUsageProgressBar.setValue(new Random().nextInt(0, 100));
+//                beschikbaarheidProgressBar.setValue(new Random().nextInt(0, 100));
+//                procesbelastingProgressBar.setValue(new Random().nextInt(0, 100));
+//                try {
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
 
 
         frame.add(diskUsageProgressBar, "skip 2");
@@ -93,18 +77,70 @@ public class MonitorGUI {
 
             } catch (Exception t) {
                 t.printStackTrace();
-                JOptionPane.showMessageDialog(frame, "kankerzooi");
+                JOptionPane.showMessageDialog(frame, "Er is een fout opgetreden!");
             }
-        });//.run();
+        }).start();
+
+        new Thread(() -> {
+            int totalTicks = 0;
+            int ticksAlive = 0;
+
+            while(true){
+                if(!serverTime.isEmpty()){
+                    long time = serverTime.values().iterator().next();
+                    long delta = System.currentTimeMillis() - time;
+
+                    if(delta < 3000){
+                        ticksAlive++;
+                    }
+                }
+
+                totalTicks++;
+                double beschikbaarheid = (ticksAlive * 100d) / totalTicks;
+                beschikbaarheidProgressBar.setValue((int) beschikbaarheid);
+
+                beschikbaarheidProgressBar.setString("Beschikbaarheid " + String.format("%.2f", beschikbaarheid) + " %");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
         frame.setVisible(true);
     }
 
+
+
     public static void handleClient(Socket socket) {
+        serverTime.put(socket, System.currentTimeMillis());
+
         try {
             var input = new Scanner(new BufferedInputStream(socket.getInputStream()));
             while (true) {
-                System.out.println("Binnen: " + input.nextLine());
+                while (!input.hasNextLine()) {
+                    Thread.yield();
+                }
+//                System.out.println("Binnen: " + input.nextLine());
+                String lijn = input.nextLine();
+                String[] parts = lijn.split(" ");
+
+                if (parts.length != 2){
+                    JOptionPane.showMessageDialog(null, "Een verouderde monitorserver probeert contact te maken");
+                    return;
+                }
+                double processorbelasting = (Double.parseDouble(parts[0])*100);
+                double diskUsage = (Double.parseDouble(parts[1])*100);
+
+                procesbelastingProgressBar.setValue((int) processorbelasting);
+                procesbelastingProgressBar.setString("Procesbelasting " + String.format("%.2f", processorbelasting) + " %");
+
+                diskUsageProgressBar.setValue((int) diskUsage);
+                diskUsageProgressBar.setString("Gebruikt " + String.format("%.2f", diskUsage) + " %");
+
+                serverTime.put(socket, System.currentTimeMillis());
             }
         } catch (Exception e) {
             e.printStackTrace();
